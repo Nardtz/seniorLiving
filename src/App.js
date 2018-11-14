e8e16a0fb7470142c9893932656612419111c07d
Nardtz <cruzialmethod@gmail.com>
2018-11-13 18:23:14 -0800

    Added Normalize, section, colmun.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Column from "./Column/Column";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <section className="Section">
+          <h1 className="logo">Your Logo</h1>
+        </section>
+        <section className="Section Section--blue">
+          <h1 className="topHead">
+            Find Assisted Living Costs Near undefined, undefined
+          </h1>
+          <Column />
+        </section>
       </div>
     );
   }
